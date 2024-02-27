258e047809c047d1929ace6622c582471b83596a
vlrlmn <lomakina.valerie@gmail.com>
2024-02-27 12:03:06 +0400

    add

--- push_swap_algorithm.c
+++ push_swap_algorithm.c
@@ -1,6 +1,6 @@
 #include "push_swap.h"
 
-void save_index(t_stack *a, int value, t_stack *ptr, t_stack *highest)
+void save_index(t_stack *a, int value, t_stack *ptr, t_stack **highest)
 {
     while(ptr)
     {
@@ -9,7 +9,7 @@ void save_index(t_stack *a, int value, t_stack *ptr, t_stack *highest)
         if (ptr->num > value && ptr->index == 0)
         {
             value = ptr->num;
-            highest = ptr;
+            *highest = ptr;
             ptr = a;
         }
         else
@@ -28,17 +28,44 @@ void  get_index(t_stack *a, int stack_len)
         ptr = a;
         value = INT_MIN;
         highest  = NULL;
-        save_index(a, value, ptr, highest);
+        save_index(a, value, ptr, &highest);
         stack_len--;
-        if(!highest)
+        if(highest != NULL)
             highest->index = stack_len;
+    }
+}
+
+void push_to_three(t_stack **a, t_stack **b, int stack_len)
+{
+    int pushed;
+    int middle;
+    int i;
+
+    pushed = 0;
+    i = 0;
+    middle = stack_len / 2;
+    while(stack_len > 6 && i < stack_len && pushed < middle)
+    {
+        if ((*a)->index <= middle)
+        {
+            pb(a, b);
+            pushed++;
+        }
+        else
+            ra(a);
+        i++;
+    }
+    while(stack_len - pushed > 3)
+    {
+        pb(a, b);
+        pushed++;
     }
 }
 
 void sort_stacks(t_stack **a, t_stack **b, int stack_len)
 {
     get_index(a, stack_len);
-    push_to_three(a, b);
+    push_to_three(a, b, stack_len);
     sort_three(a);
     sort_b(a, b);
     if(!stack_sorted(*a))
